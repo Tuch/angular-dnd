85d5568ef75d8ef20468236f1d88f9f2b0f56b81
Alex Afonin <toafonin@gmail.com>
2016-07-20 13:57:00 +0200

    0.1.24

--- dist/angular-dnd.js
+++ dist/angular-dnd.js
@@ -2,7 +2,7 @@
 
 
 /**
-* @license AngularJS-DND v0.1.23
+* @license AngularJS-DND v0.1.24
 * (c) 2014-2015 Alexander Afonin (toafonin@gmail.com, http://github.com/Tuch)
 * License: MIT
 */
@@ -19,7 +19,7 @@
 
 /* ENVIRONMENT VARIABLES */
 
-var version = '0.1.23',
+var version = '0.1.24',
     $ = angular.element, $window = $(window), $document = $(document), body = 'body', TRANSFORM, TRANSFORMORIGIN, MATCHES_SELECTOR,
     debug = {
         mode: false,
--- src/core.js
+++ src/core.js
@@ -1,6 +1,6 @@
 
 /**
-* @license AngularJS-DND v0.1.23
+* @license AngularJS-DND v0.1.24
 * (c) 2014-2015 Alexander Afonin (toafonin@gmail.com, http://github.com/Tuch)
 * License: MIT
 */
@@ -17,7 +17,7 @@
 
 /* ENVIRONMENT VARIABLES */
 
-var version = '0.1.23',
+var version = '0.1.24',
     $ = angular.element, $window = $(window), $document = $(document), body = 'body', TRANSFORM, TRANSFORMORIGIN, MATCHES_SELECTOR,
     debug = {
         mode: false,
